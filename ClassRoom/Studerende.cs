ef13c3b44aeba6220a80f73ac68cf7fab49af77a
Mervan <Merv0084@edu.easj.dk>
2018-01-30 19:47:15 +0100

    ClassRoom Studerende

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        public string Navn { get; }
+        public int Fødselsmåned { get; }
+        public int Fødselsdag { get; }
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            Navn = navn;
+            Fødselsmåned = fødselsmåned;
+            Fødselsdag = fødselsdag;
+        }
+    }
+    
+
+
+    
+
+    
+}
