810236ed068b8099c067957c4d9465b18da75806
Mervan <Merv0084@edu.easj.dk>
2018-01-30 19:53:40 +0100

    ClassRoom Studerende Done

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+   public class KlasseRum
+    {
+        public string KlasseNavn { get; }
+        public List<Studerende> KlasseListe { get; }
+        public DateTime SemesterStart { get; }
+
+        public KlasseRum()
+        {
+            
+        }
+
+    
+    }
+}
